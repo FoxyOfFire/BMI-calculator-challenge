94553a4cb58e893de2e3cc63238a65acb1dc42ec
FoxyOfFire <36312390+FoxyOfFire@users.noreply.github.com>
2025-03-20 12:19:36 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function bmiCalculator(weight, height) {
+
+    return weight / Math.pow(height, 2);
+    Math.round(bmiCalculator(weight, height));
+
+    var bmi = bmiCalculator(65, 1.8);
+    
+    return bmi;
+  
+}
